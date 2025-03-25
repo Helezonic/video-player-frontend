202bc92c6ea136cd5ccd265a459d2dfda4c8687c
Shuaib <helezon.dev@gmail.com>
2025-03-25 15:12:33 +0530

    Vite reconfig

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,6 @@ import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
-  servers: {
-    https : {
-      key: fs.readFileSync('/home/helezon/Fullstack/video-player/frontend/video-player-react/cert.key'),
-      cert : fs.readFileSync('/home/helezon/Fullstack/video-player/frontend/video-player-react/cert.crt')
-    }
-  },
   plugins: [
     react(), 
     tailwindcss()
